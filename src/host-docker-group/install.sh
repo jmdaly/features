e8c5f570c2d38308ccbfe998b5150e7a176aa697
John M Daly <jmdaly@gmail.com>
2024-10-14 12:08:18 -0400

    update variable name

--- src/host-docker-group/install.sh
+++ src/host-docker-group/install.sh
@@ -5,7 +5,7 @@ echo "Activating feature 'host-docker-group'"
 
 # The 'install.sh' entrypoint script is always executed as the root user.
 
-username="${CONTAINERUSER}"
+container_user="${CONTAINERUSER}"
 container_docker_group="${CONTAINERDOCKERGROUP}"
 
 groupadd --system $container_docker_group
@@ -18,8 +18,8 @@ tee /usr/local/share/docker-init.sh > /dev/null \
 set -e
 
 # Only execute the below code if the user exists
-if id -u $username &>/dev/null; then
-	echo "Ensuring $username is in the $container_docker_group group"
+if id -u $container_user &>/dev/null; then
+	echo "Ensuring $container_user is in the $container_docker_group group"
 	# Modify the newly created group to have the same GID as the host's docker group
 	host_docker_group_gid=\$(stat -c %g /var/run/docker.sock)
 
@@ -27,7 +27,7 @@ if id -u $username &>/dev/null; then
 	# GID is already in use.
 	sudo groupmod -g \$host_docker_group_gid -o $container_docker_group
 	# Add the user to the new group.
-	sudo usermod -aG $container_docker_group $username
+	sudo usermod -aG $container_docker_group $container_user
 fi
 
 # Execute whatever commands were passed in (if any). This allows us
