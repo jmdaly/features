6e0bbea4a57fddbeebc84dfde45a740a7f6bac77
John M Daly <jmdaly@gmail.com>
2025-04-08 22:25:02 -0400

    feat: add neovim-bin feature for aarch64 and x86_64 (#17)

---
+++ src/neovim-bin/README.md
@@ -0,0 +1,24 @@
+
+# Neovim (neovim-src)
+
+Neovim is a fork of Vim focused on modern code and features, rather than running in legacy environments.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/jmdaly/features/neovim-src:1": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | The version of Neovim to be installed (nightly or nightly) | string | nightly |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/jmdaly/features/blob/main/src/neovim-src/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
--- src/neovim-bin/install.sh
+++ src/neovim-bin/install.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-set -e
+set -ex
 
 echo "Activating feature 'neovim'"
 
-VERSION=${VERSION:-stable}
+NVIM_VERSION=${NVIMVERSION:-latest}
 
 # The 'install.sh' entrypoint script is always executed as the root user.
 
 install_debian_dependencies() {
   echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
   apt-get update
-  apt-get install -y ninja-build gettext cmake unzip curl build-essential
+  apt-get install -y curl
 
   apt-get clean && rm -rf /var/lib/apt/lists/*
 }
 
-ADJUSTED_VERSION=$VERSION
-
-if [  "$VERSION" != "stable" ] && [  "$VERSION" != "nightly" ]; then
-    ADJUSTED_VERSION="v$VERSION"
+# Get the system's architecture with uname. If it is aarch64, set the architecture to arm64.
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then
+  ARCH="arm64"
 fi
 
 # Detemine the distro we're on by using /etc/os-release
@@ -41,14 +41,14 @@ else
   exit 1
 fi
 
-echo "Downloading neovim $ADJUSTED_VERSION"
+echo "Installing neovim $NVIM_VERSION"
+FILENAME="nvim-linux-$ARCH.tar.gz"
 
-curl -sL https://github.com/neovim/neovim/archive/refs/tags/${ADJUSTED_VERSION}.tar.gz | tar -xzC /tmp 2>&1
+# The URLs are slightly different for the latest and nightly versions.
+if [ "$NVIM_VERSION" = "latest" ]; then
+  FILENAME="$NVIM_VERSION/download/$FILENAME"
+elif [ "$NVIM_VERSION" = "nightly" ]; then
+  FILENAME="download/$NVIM_VERSION/$FILENAME"
+fi
 
-echo "Building neovim $ADJUSTED_VERSION"
-cd /tmp/neovim-${ADJUSTED_VERSION}
-make CMAKE_BUILD_TYPE=Release
-make install
-
-echo "Cleaning up"
-rm -rf /tmp/neovim-${ADJUSTED_VERSION}
+curl -sL https://github.com/neovim/neovim/releases/$FILENAME | tar --strip-components=1 -xvz -C /usr/local
